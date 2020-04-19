6399f18a87bddbfdbd08c9ee845440306f11ce47
Eugene Yong <yonge@oregonstate.edu>
2020-04-18 18:08:21 -0700

    Added successor function

--- assignment_1.cpp
+++ assignment_1.cpp
@@ -4,6 +4,7 @@
 #include <cstdlib>
 #include <string>
 #include <sstream>
+#include <vector>
 #include <fstream>
 
 using namespace std;
@@ -30,7 +31,7 @@ void valid_argc(int argc){
   }
 }
 
-void print_state(struct State& state){
+void print_state(struct State state){
   cout << state.l_bank.chickens << ", " << state.l_bank.wolves << ", " << state.l_bank.boat << endl;
   cout << state.r_bank.chickens << ", " << state.r_bank.wolves << ", " << state.r_bank.boat << endl;
 }
@@ -60,24 +61,65 @@ void read_file(int index, char* argv[], struct State& state){
 
 // actions to generate successor according to act given in arguments
 void actions(struct State& state, int act){
-  if(act == 1){
+  if(act == 0){
     // Put one chickens in the boat
-  }else if(act == 2){
+    state.r_bank.chickens -= 1;
+    state.l_bank.chickens += 1;
+  }else if(act == 1){
     // Put two chickens in the boat
-  }else if(act == 3){
+    state.r_bank.chickens -= 2;
+    state.l_bank.chickens += 2;
+  }else if(act == 2){
     // Put one wolf in the boat
-  }else if(act == 4){
+    state.r_bank.wolves -= 1;
+    state.l_bank.wolves += 1;
+  }else if(act == 3){
     // Put one wolf one chicken in the boat
-  }else if(act == 5){
+    state.r_bank.wolves -= 1;
+    state.r_bank.chickens -= 1;
+    state.l_bank.wolves += 1;
+    state.l_bank.chickens += 1;
+  }else if(act == 4){
     // Put two wolves in the boat
+    state.r_bank.wolves -= 2;
+    state.l_bank.wolves += 2;
   }
+  state.l_bank.boat = !state.l_bank.boat;
+  state.r_bank.boat = !state.r_bank.boat;
+}
+
+bool check_state(struct State state){
+  if(state.l_bank.chickens < 0 || state.r_bank.chickens < 0 || state.l_bank.wolves < 0 || state.r_bank.wolves < 0){
+    return false;
+  }
+  if(state.l_bank.chickens != 0){
+    if(state.l_bank.chickens < state.l_bank.wolves){
+      return false;
+    }
+  }
+  if(state.r_bank.chickens != 0){
+    if(state.r_bank.chickens < state.r_bank.wolves){
+      return false;
+    }
+  }
+  return true;
 }
 
 // Generate successor states reachable from state
-struct State* succ(struct State state){
+ vector<struct State> succ(struct State state){
   struct State states[5];
-  struct State* successors;
+  vector<struct State> successors;
+  for(int i = 0; i < 5; i++){
+    states[i] = state;
+    states[i].parent = &state;
+  }
   // Do action to each successor, put into successors if valid
+  for(int i = 0; i < 5; i++){
+    actions(states[i], i);
+    if(check_state(states[i]) == true){
+      successors.push_back(states[i]);
+    }
+  }
   return successors;
 }
 
@@ -93,8 +135,12 @@ int main(int argc, char* argv[]){
   valid_argc(argc);
   struct State init_state;
   struct State goal_state;
+  vector<struct State> expanded;
   read_file(1, argv, init_state);
   read_file(2, argv, goal_state);
-  succ(init_state);
+  expanded = succ(init_state);
+  for(int i = 0; i < expanded.size(); i++){
+    print_state(expanded[i]);
+  }
   return 0;
 }
